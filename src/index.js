1a7ca32e0e2f84fae9d12c7b92430d8830c430e5
Mousa Ayoubi <mousaayoubi@gmail.com>
2020-09-23 15:31:01 +0000

    Update files

--- src/index.js
+++ src/index.js
@@ -3,12 +3,33 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = `
   type Query {
 	  info: String!
+	  feed: [Link!]!
+  }
+
+  type Link {
+	  id: ID!
+	  description: String!
+	  url: String!
   }
 `;
 
+let links = [{
+	id: 'link-0',
+	url: 'www.howtographql.com',
+	description: 'Fullstack tutorial for GraphQL'
+}];
+
+
 const resolvers = {
 	Query: {
-		info: () => null,
+		info: () => `This is the API of a Hackernews clone`,
+		feed: () => links,
+	},
+
+	Link: {
+		id: (parent) => parent.id,
+		description: (parent) => parent.description,
+		url: (parent) => parent.url,
 	}
 };
 
