d0741476da513cc392d1d7a977e697cc5123b3c1
Mousa Ayoubi <mousaayoubi@gmail.com>
2020-09-24 15:12:52 +0000

    Update files

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const User = require('./resolvers/User');
 const Link = require('./resolvers/Link');
+const Subscription = require('./resolvers/Subscription');
+const { PubSub } = require('graphql-yoga');
+
+const pubsub = new PubSub();
 
 const prisma = new PrismaClient();
 
 const resolvers = {
 	Query,
 	Mutation,
+	Subscription,
 	User,
 	Link,
 };
@@ -21,6 +26,7 @@ const server = new GraphQLServer({
 		return {
 			...request,
 			prisma,
+			pubsub,
 		}
 	},
 });
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -6,13 +6,16 @@ function post(parent, args, context, info) {
 
 	const userId = getUserId(context);
 
-	return context.prisma.link.create({
+	const newLink = context.prisma.link.create({
 		data: {
 			url: args.url,
 			description: args.description,
 			postedBy: { connect: { id: userId } },
 		}
-	})
+	});
+	context.pubsub.publish("NEW_LINK", newLink);
+
+	return newLink;
 };
 
 async function signup(parent, args, context, info) {
